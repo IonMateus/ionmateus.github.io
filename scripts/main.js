2275df0cbda3a2311768b9237b892963f85d9238
Ion Mateus <ion.mateus.oprea@gmail.com>
2024-02-01 13:59:06 -0300

    more options menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,25 @@
 
-function toggleHeaderLinks() {
-    const headerLinks = document.getElementById('headerLinks');
-    if (headerLinks) {
-      headerLinks.classList.toggle('newHeaderLinks');
-    }
-  }
-
-window.onload = function(){
-  if (window.innerWidth <= 600) {
-    toggleHeaderLinks();
-  } else {
-    headerLinks.classList.remove('newHeaderLinks');
-  }
+function CloseHeaderMoreOptions(){
+  let headerLinks = document.getElementById("headerLinks")
+  headerLinks.style.display = 'none'
+  document.getElementById("OpenHeaderMoreOptions").style.display = "block"
 }
 
+function OpenHeaderMoreOptions(){
+  let headerLinks = document.getElementById("headerLinks")
+  headerLinks.style.display = 'block'
+  headerLinks.style.display = "flex"
+  document.getElementById("OpenHeaderMoreOptions").style.display = "none"
+}
 
+function ChangeHeaderMoreOptions(){
+  let headerLinks = document.getElementById("headerLinks")
+  let status = headerLinks.style.display
+  if(status == 'flex'){
+    headerLinks.style.display = 'none'
+    document.getElementById("OpenHeaderMoreOptions").style.display = "block"  
+  }else{
+    headerLinks.style.display = "flex"
+    document.getElementById("OpenHeaderMoreOptions").style.display = "none"
+  }
+}
