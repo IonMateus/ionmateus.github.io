09f3421805da347881889f7413c8f971f43e575b
Ion Mateus <ion.mateus.oprea@gmail.com>
2024-04-20 17:42:45 -0300

    7th tent

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,48 @@
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $smallBallBlack = document.querySelector('.cursor__ball--small-black');
+const $smallBallWhite = document.querySelector('.cursor__ball--small-white');
+const $hoverables = document.querySelectorAll('.hoverable');
 
-function CloseHeaderMoreOptions(){
-  let headerLinks = document.getElementById("headerLinks")
-  headerLinks.style.display = 'none'
-  document.getElementById("OpenHeaderMoreOptions").style.display = "block"
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
 }
 
-function OpenHeaderMoreOptions(){
-  let headerLinks = document.getElementById("headerLinks")
-  headerLinks.style.display = 'block'
-  headerLinks.style.display = "flex"
-  document.getElementById("OpenHeaderMoreOptions").style.display = "none"
+// Move the cursor
+function onMouseMove(e) {
+  TweenMax.to($bigBall, .4, {
+    x: e.pageX - 15,
+    y: e.pageY - 15
+  })
+  TweenMax.to($smallBallBlack, .0, {
+    x: e.pageX - 5,
+    y: e.pageY - 3
+  })
+  TweenMax.to($smallBallWhite, .0, {
+    x: e.pageX - 5,
+    y: e.pageY - 5
+  })
 }
 
-function ChangeHeaderMoreOptions(){
-  let headerLinks = document.getElementById("headerLinks")
-  let status = headerLinks.style.display
-  if(status == 'flex'){
-    headerLinks.style.display = 'none'
-    document.getElementById("OpenHeaderMoreOptions").style.display = "block"  
-  }else{
-    headerLinks.style.display = "flex"
-    document.getElementById("OpenHeaderMoreOptions").style.display = "none"
-  }
+function changeTheme() {
+    const color1 = getComputedStyle(document.documentElement).getPropertyValue('--color1');
+    const color2 = getComputedStyle(document.documentElement).getPropertyValue('--color2');
+    
+    document.documentElement.style.setProperty('--color1', color2);
+    document.documentElement.style.setProperty('--color2', color1);
+}
+
+
+// Hover an element
+function onMouseHover() {
+  TweenMax.to($bigBall, .3, {
+    scale: 4
+  })
+}
+function onMouseHoverOut() {
+  TweenMax.to($bigBall, .3, {
+    scale: 1
+  })
 }
